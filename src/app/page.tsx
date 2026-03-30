69745df0cf7582fb0fe7159b01e0c5637d4bfd15
fhranco <patagoniacoach@gmail.com>
2026-03-29 22:48:04 -0300

    🚀 UX: Removed entry barrier. Name is now requested at checkout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,11 @@ import React from "react";
 import { Navigation } from "@/components/Navigation";
 import { MenuExplorer } from "@/components/MenuExplorer";
 import { TrayModal } from "@/components/TrayModal";
-import { WelcomeOverlay } from "@/components/WelcomeOverlay";
+
 
 export default function Home() {
   return (
     <div className="relative w-full h-[100dvh]">
-      {/* 1. INITIAL CHECK-IN */}
-      <WelcomeOverlay />
-
       {/* 2. THE NAVIGATION COMPASS */}
       <Navigation />
       
